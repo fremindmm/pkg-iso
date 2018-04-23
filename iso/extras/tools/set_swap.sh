e90da9364d7263d5228c7a672ed6ea066890ec22
jeckxie01 <xiaozhe561@126.com>
2018-04-24 03:18:11 +0800

    add set swap script,and fix sync file , genrate plan_table bug

---
+++ iso/extras/tools/set_swap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -o xtrace
+
+SWAP_SIZE=8192
+function auto_swap_size(){
+    phy_mem=`free -m |grep "Mem"|awk -F' ' '{print $2}'`
+    if [ $phy_mem -ge 4095 -a $phy_mem -lt 16384 ];then
+        SWAP_SIZE=8192
+    elif [ $phy_mem -ge 16384 -a $phy_mem -lt 65536 ];then
+        SWAP_SIZE=16384
+    elif [ $phy_mem -ge 65536 ];then
+        SWAP_SIZE=32768
+    else
+        SWAP_SIZE=8192
+    fi
+}
+
+
+
+function setup_swap(){
+    if [ ! -f /swapfile ]; then
+        sudo swapoff -a
+        sudo dd if=/dev/zero of=/swapfile bs=1M count=$1
+        sudo chmod 0600 /swapfile
+        sudo mkswap /swapfile
+        sudo /sbin/swapon /swapfile
+    fi
+}
+setup_swap $SWAP_SIZE
