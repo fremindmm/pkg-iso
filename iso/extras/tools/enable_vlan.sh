a52b96624a6cc55cc29e777af3dc6e138bd93ce9
jeckxie01 <xiaozhe561@126.com>
2018-07-31 07:29:29 +0800

    Add exception handling for performing add bridge operations

--- iso/extras/tools/enable_vlan.sh
+++ iso/extras/tools/enable_vlan.sh
@@ -26,7 +26,10 @@ fi
 # Config container neutron_openvswitch_agent and container neutron_server
 # **************************************************************************************
 OPENVSWITCH_PATH="/etc/kolla/neutron-openvswitch-agent"
-OPENVSWITCH_ID=`docker ps |grep neutron-openvswitch-agent|awk -F' ' '{print $1}'`
+OPENVSWITCH_ID=`docker ps |grep Up|grep neutron-openvswitch-agent|awk -F' ' '{print $1}'`
+if [ ! -n ${OPENVSWITCH_ID} ];then
+OPENVSWITCH_ID=`docker ps |grep Up|grep centos-source-openvswitch-vswitchd|awk -F' ' '{print $1}'`
+fi
 #NEUTRON_SERVER_PATH="/etc/kolla/neutron-server"
 #NEUTRON_SERVER_ID=`docker ps |grep neutron-server|awk -F' ' '{print $1}'`
 echo ${OPENVSWITCH_ID}
