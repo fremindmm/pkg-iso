7d853a9fe10f2bc2b22aa5144004f5a136e43a56
fremindmm <fremindmm@users.noreply.github.com>
2019-07-08 20:30:56 +0800

    Update resize_vdn.sh
    use uuid for disk fstab

--- iso/extras/tools/resize_vdn.sh
+++ iso/extras/tools/resize_vdn.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 DIR="/data"
-DISK="vdb"
+DISK=$1
 FORMART="ext4"
 mkdir -p ${DIR}
 function part_disk_and_init_formart(){
     local DISK=$1
     local DIR=$2
-    local FORMART=$3 
+    local FORMART=$3
     echo "n
     p
     1
-    
-    
+
+
     w
     " | fdisk /dev/${DISK} && partprobe
     mkfs -t ${FORMART} /dev/${DISK}1
@@ -21,8 +21,9 @@ mount /dev/${DISK}1 ${DIR}/
 df -h
 umount /data
 df -h
-cat << EOF > /etc/fstab
-/dev/${DISK}1  ${DIR}  ${FORMART}  defaults  1  2
+UUID=`blkid|grep ${DISK}1|awk -F' ' '{print $2}'`
+cat << EOF >> /etc/fstab
+$UUID  ${DIR}  ${FORMART}  defaults  1  2
 EOF
 mount -a
 df -h
